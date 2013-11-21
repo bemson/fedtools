a2faae08db778f85bbe34d16cee5b6e597708a2e
Arno V <versini@gmail.com>
2013-11-21 12:26:04 -0800

    Updating release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
 
   // project configuration
   grunt.initConfig({
+
     release: {
       options: {
         bump: true,
@@ -15,10 +16,24 @@ module.exports = function (grunt) {
         npm: false
       }
     }
+
   });
 
   // load plugins
   grunt.loadNpmTasks('grunt-release');
   // register running tasks
-  grunt.registerTask('default', ['release']);
+  grunt.registerTask('default', ['help']);
+
+  grunt.registerTask('help', 'Display help usage', function () {
+    console.log();
+    console.log('Type grunt release to:');
+
+    console.log(' - bump the version in package.json file.');
+    console.log(' - stage the package.json file\'s change.');
+    console.log(' - commit that change.');
+    console.log(' - create a new git tag for the release.');
+    console.log(' - push the changes out to github.');
+    console.log(' - push the new tag out to github.');
+    console.log(' - publish the new version to npm.');
+  });
 };
