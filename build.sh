d565f2afd9afbc29e484d87627bdf7a17cd81bf8
Arno V <versini@gmail.com>
2013-11-21 10:58:25 -0800

    Removing obsolete build script

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PWD=`pwd`
-path=$(basename $PWD)
-rm -rf archive/fedtools.tgz
-tar --exclude='.git' --exclude='node_modules' --exclude='build.sh' --exclude='archive' -cvzf archive/fedtools.tgz .
